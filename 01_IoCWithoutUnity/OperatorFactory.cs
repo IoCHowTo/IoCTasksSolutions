0365af0a1a7c48a5dc61141bfd3f16547bf59819
michalhosala <michal.hosala@gmail.com>
2014-11-28 17:31:35 +0100

    task 1 solution project + readme

---
+++ 01_IoCWithoutUnity/OperatorFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IoCWithoutUnity
+{
+    public interface IOperationFactory
+    {
+        IOperation Create(string token);
+    }
+
+    public class OperationFactory : IOperationFactory
+    {
+        private readonly IContainer m_Container;
+
+        public OperationFactory(IContainer container)
+        {
+            m_Container = container;
+        }
+
+        public IOperation Create(string token)
+        {
+            switch (token)
+            {
+                case "+":
+                    return (IOperation)m_Container.Create("IOperationPlus");
+                case "-":
+                    return (IOperation)m_Container.Create("IOperationMinus");
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
