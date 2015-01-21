16825dd66b2ddd30aa3efed0055e61892ceb49a6
voloda <vladimir.kloz@gmail.com>
2015-01-21 23:27:54 +0100

    Issue #9: Create a project how to deal with parameterized constructor

--- 110_UnityResolutionOverrides/Program.cs
+++ 110_UnityResolutionOverrides/Program.cs
@@ -18,8 +18,27 @@ namespace UnityResolutionOverrides
                 var calculator = container.Resolve<ICalculator>();
                 int result = calculator.Evaluate(args);
 
-                // TODO: Get the resolution working using various types of dependency overrides
-                var resultWriter = container.Resolve<IConsoleAndFileResultWriter>();
+                // This is one option for fix - please note that the scope of the ParameterOverride is not limited
+                // and thus may apply to resolutions of dependencies
+                var resultWriter = container.Resolve<IConsoleAndFileResultWriter>(
+                    new ParameterOverride("fileName", "output.txt"),
+                    new ParameterOverride("context", "Context data")
+                    );
+
+                // Another option is to limit the scope to the type being built. The downside of this solution is that you are making
+                // code tightly coupled with concrete implementation thus be very careful and do not spread this into multiple places
+                //var resultWriter = container.Resolve<IConsoleAndFileResultWriter>(
+                //    new ParameterOverride("fileName", "output.txt").OnType<ConsoleAndFileResultWriter>(),
+                //    new ParameterOverride("context", "Context data").OnType<ConsoleAndFileResultWriter>()
+                //    );
+
+                // Another option is to override a type and to limit the scope to the type being built. 
+                // The downside of this solution is that you are making code tightly coupled with concrete 
+                // implementation thus be very careful and do not spread this into multiple places
+                // Also keep in mind that this cannot be used if multiple parameters of the same type can be used (see the ConsoleAndFileResultWriter
+                // constructor and also the written file)
+                //var resultWriter = container.Resolve<IConsoleAndFileResultWriter>(new DependencyOverride(typeof(string), "output.txt")
+                //        .OnType<ConsoleAndFileResultWriter>());
 
                 resultWriter.WriteResult(result);
             }
